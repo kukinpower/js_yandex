d545f2761ed91463c3811cf9067e5cdf9cdce350
kukinpower <thekukinpower@gmail.com>
2020-12-06 04:18:31 +0300

    solve week 4 assignement

--- 01_JavaScript_part1/week4/01_query/index.js
+++ 01_JavaScript_part1/week4/01_query/index.js
@@ -4,14 +4,38 @@
  * @returns {Array}
  */
 function query(collection) {
-
+    var clone = JSON.parse(JSON.stringify(collection));
+    console.log(clone);
+    if (arguments.length === 1)
+        return clone;
+    if (arguments.length > 1)
+    {
+        for (var i = arguments.length - 1; i > 0; i--) {
+            clone = arguments[i](clone);
+        }
+        return clone;
+    }
+    return [];
 }
 
 /**
  * @params {String[]}
  */
 function select() {
+    var args = [].slice.call(arguments);
 
+    return function select(collection) {
+        var ret = [];
+
+        for (var i = 0; i < collection.length; i++) {
+            var obj = {};
+            for (var j = 0; j < args.length; j++) {
+                obj[args[j]] = collection[i][args[j]];
+            }
+            ret.push(obj);
+        }
+        return ret;
+    }
 }
 
 /**
@@ -19,7 +43,19 @@ function select() {
  * @param {Array} values – Массив разрешённых значений
  */
 function filterIn(property, values) {
+    return function filterIn(collection) {
+        var ret = [];
 
+        for (var i = 0; i < collection.length; i++) {
+            for (var j = 0; j < values.length; j++) {
+                if (collection[i][property] === values[j]) {
+                    ret.push(collection[i]);
+                    break ;
+                }
+            }
+        }
+        return ret;
+    }
 }
 
 module.exports = {
